99616ac5a965105ec03a0e297ec8dec3a22defbb
Martin Tonnier <martin@tonnier.name>
2022-03-04 20:07:46 +0100

    Update build.sh

--- docker/dockerfiles/build.sh
+++ docker/dockerfiles/build.sh
@@ -44,14 +44,14 @@ case $version in
         docker buildx build \
                --platform="$platforms" $push \
                --build-arg restserver_url="$url_base/docspell-restserver-$version.zip" \
-               --tag mtonnie/docspell-restserver:coretto \
+               --tag mtonnie/docspell-restserver:openjdk8 \
                -f restserver.dockerfile .
 
         echo "============ Building Joex ============"
         docker buildx build \
                --platform="$platforms" $push \
                --build-arg joex_url="$url_base/docspell-joex-$version.zip" \
-               --tag mtonnie/docspell-joex:coretto \
+               --tag mtonnie/docspell-joex:openjdk8 \
                -f joex.dockerfile .
         ;;
     *)
